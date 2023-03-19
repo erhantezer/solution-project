94867339ee7dff17cbd0be6b2ef62721a9bc1194
Erhan <erhantezer68@gmail.com>
2023-03-19 20:43:21 +0300

    updated

---
+++ src/context.js
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState } from "react";
+
+
+const AppContext = createContext();
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+};
+
+export const AppProvider = ({children}) => {
+    const [filmIndex, setFilmIndex] = useState(0)
+
+
+
+    return (
+        <AppContext.Provider value={{ filmIndex, setFilmIndex }}>
+                {children}
+            </AppContext.Provider>
+        )
+}
