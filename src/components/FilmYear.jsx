b977851368a9f3907fc441698e3fb7b9809f0163
Erhan <erhantezer68@gmail.com>
2023-03-20 11:47:07 +0300

    updated

--- src/components/FilmYear.jsx
+++ src/components/FilmYear.jsx
@@ -1,18 +1,25 @@
 import { useGlobalContext } from '../context';
 
-const FilmYear = ({data}) => {
+const FilmYear = ({ data }) => {
     const { filmIndex, setFilmIndex } = useGlobalContext();
 
     return (
         <div className='col-12 col-lg-1'>
-            <div>
-                {data.map((item)=> {
+            <div
+                className="nav nav-pills d-flex align-items-start "
+                id="v-pills-tab"
+                role="tablist"
+                aria-orientation="vertical">
+                {data.map((item) => {
                     return (
-                        <button 
-                        className={`border-0 px-2 m-2 fs-4 fw-bold text-secondary ${item.id===filmIndex && "activeBtn"}`} onClick={() => setFilmIndex(item.id)}>
-                                {item.date}
-                            </button>
-                        )
+                        <button
+                            className={`border-0 px-2 m-2 fs-4 fw-bold text-secondary 
+                        ${item.id === filmIndex && "activeBtn"}`}
+                            onClick={() => setFilmIndex(item.id)}
+                        >
+                            {item.date}
+                        </button>
+                    )
                 })}
             </div>
         </div>
