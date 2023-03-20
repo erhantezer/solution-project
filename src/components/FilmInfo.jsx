5f560a8cc8ae19ba5b7e2c5b88f7b585d418149c
Erhan <erhantezer68@gmail.com>
2023-03-20 11:55:21 +0300

    updated

--- src/components/FilmInfo.jsx
+++ src/components/FilmInfo.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 
 const FilmInfo = ({ film }) => {
-    const { id, title, body, rate } = film
+    const { id, title, body, rate, tags } = film
 
     return (
-        <div key={id}>
-            {title}
+        <div key={id} className='col-12 col-sm-9'>
+            <div>
+                <h3>{title}</h3>
+                <span>{body}</span>
+            </div>
+            
         </div>
     )
 }
